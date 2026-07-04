6d32de776b6a0f7a30c54aeff40636f3215ba8ad
sabaoqropiridze2003-create <sabaoqropiridze2003@gmail.com>
2026-07-04 17:36:22 +0400

    Add files via upload

---
+++ filehandling.py
@@ -0,0 +1,51 @@
+# f = open("filehandling/demofile.txt")
+
+# print(f.read())
+
+
+# with open("filehandling/demofile.txt") as f:
+#     print(f.read())
+
+# f = open("filehandling/demofile.txt")
+# print(f.read())
+# f.close()
+
+# with open("filehandling/demofile.txt") as f:
+#     print(f.read(5))
+
+# with open("filehandling/demofile.txt") as f:
+#     print(f.readline())
+#     print(f.readline())
+#     print(f.readline())
+
+
+# with open("filehandling/demofile.txt") as f:
+#     for x in f:
+#         print(x)
+
+# with open("filehandling/demofile.txt", "a") as f:
+#     f.write("\nNow the file has more content")
+
+# with open("filehandling/demofile.txt") as f:
+#     print(f.read())
+
+# with open("filehandling/demofile.txt", "w") as f:
+#     f.write("oops i have deleted the content")
+
+# with open("filehandling/demofile.txt", "r") as f:
+#     print(f.read())
+
+# f = open("filehandling/myfile.txt", "x")
+
+
+import os
+
+# os.remove("filehandling/myfile.txt")
+
+
+# if os.path.exists("filehandling/demofile.txt"):
+#     os.remove("filehandling/demofile.txt")
+# else:
+#     print("this file does not exist")
+
+# os.rmdir("filehandling")
